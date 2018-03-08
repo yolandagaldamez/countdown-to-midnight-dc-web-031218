e63335d79a2dcac05e8be48585aae8bf919db0e0
Yolanda Galdamez <yolanda.v.galdamez@gmail.com>
2018-03-08 00:10:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
-def countdown
- n = 10 
+def countdown(countdown_num)
+ n = countdown_num 
  while n >= 0 
   puts "#{n} SECONDS!"
   n -= 1
@@ -9,7 +9,7 @@ end
 
 
 
-def countdown_with_sleep (countdown_num)
+def countdown_with_sleep(countdown_num)
  n = countdown_num
  while n >= 0 
   puts "#{n} SECONDS!"
