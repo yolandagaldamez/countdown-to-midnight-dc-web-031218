4c0ef9421d9307a8d622c9caeb54d404aa819102
Yolanda Galdamez <yolanda.v.galdamez@gmail.com>
2018-03-08 00:12:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 def countdown(countdown_num)
  n = countdown_num 
  while n >= 0 
-  puts "#{n} SECONDS!"
+  puts "#{n} SECOND(S)!"
   n -= 1
  end
  "HAPPY NEW YEAR!"
@@ -12,7 +12,7 @@ end
 def countdown_with_sleep(countdown_num)
  n = countdown_num
  while n >= 0 
-  puts "#{n} SECONDS!"
+  puts "#{n} SECOND(S)!"
   sleep 1
   n -= 1
  end
